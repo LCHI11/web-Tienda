6e5b05c3c5a247f1ab64558da547ca0ca3804e1c
LCHI11 <luzcristinahi1@hotmail.com>
2025-06-25 13:41:51 -0500

    Add files via upload

---
+++ Proyecto/Script/Libros.js
@@ -0,0 +1,7 @@
+alert("✅ Libros.js fue cargado y ejecutado correctamente");
+console.log("📢 Este console.log proviene de Libros.js");
+console.log("🛠 Editado desde el navegador directamente");
+console.log("📢 Este console.log proviene de Libros.js");
+console.log("🛠 Editado desde el navegador directamente");
+console.log("📢 Este console.log proviene de Libros.js");
+console.log("🛠 Editado desde el navegador directamente");
